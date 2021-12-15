e13894b86526a89630a49164973e3745960a3d22
Frédéric Lopes <fred.fadiez@gmail.com>
2021-12-15 09:34:06 +0100

    Tp

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const quote = document.getElementsByClassName('quote')
-fetch('https://api.quotable.io/random')
-    .then((Res) => Res.json())
-    .then(data => {
-        console.log(`${data.content}-${data.author}`);
-    })
